e8fae96bdd6c22cc69162d244ff5e29fd3d5992c
Emmely Intriago <gabyintriago07@gmail.com>
2020-06-16 12:28:16 -0500

    practica 4

--- practicas/JavaScriptOne/scripts/librerias.js
+++ practicas/JavaScriptOne/scripts/librerias.js
@@ -1,1 +1,19 @@
-alert("hoal")
+//Controla que los elementos estén cargados
+window.addEventListener('load', function()
+{
+    //Añadir el evento para calcular la suma al boton
+    btcalcular.addEvenListener('click', function()
+    {
+    txtresultado.value= Number(txtnumero.value)+parseIint(txtnumero2.value)
+    });
+
+
+    //colocar la cadena invertida haciendo clic en el botón
+    btinvertir.addEvenListener('click', function()
+    {
+        divresultado.innerHTML= txtcadena.value.split("").reverse().join("")
+    });
+
+
+});
+
