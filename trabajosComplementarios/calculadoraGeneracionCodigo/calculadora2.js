aacd34ee33cdef384e6d4332d0d832c784d1f738
Emmely Intriago <gabyintriago07@gmail.com>
2020-06-27 10:55:27 -0500

    Calculadora 2 y avance proyecto

--- trabajosComplementarios/calculadoraGeneracionCodigo/calculadora2.js
+++ trabajosComplementarios/calculadoraGeneracionCodigo/calculadora2.js
@@ -1,29 +1,114 @@
 window.addEventListener('load', function()
 {
+    btngenerar.addEventListener("click",function(){
+    let contenidoHTML= `
+        <div><span id="divresultado"></span></div>
+        <table>`
+       
 
-    let  contenidoHTML="<table>"
-        let contador=0
-        for(i=1;i<=6;i++)
+        let l=1
+        for (i=1;i<=3;i++)
         {
             contenidoHTML+="<tr>"
-            for(j=1;j<=4;j++){
-                if(i==1 && j==1){
-                contenidoHTML+="<td colspan=4>"
-                    contenidoHTML+=`<input type = "button" value ="" id ="pantalla"/>`
-                contenidoHTML+="</td>"
-                }else{
-                    contenidoHTML+="<td>"
-                    contenidoHTML+=`<button class=btnnumero value="${i}" title="${j}"> ${++contador}</button>`
-                    contenidoHTML+="</td>"
-                }
+            for(j=1;j<=3;j++)
+            {
                 
-                    
+                contenidoHTML+="<td>"
+                contenidoHTML+= `<button class="btnnumero" value="${l}" > ${l++} </button>`
+                contenidoHTML+="</td>"
+                
             }
-            contenidoHTML+="</tr>"
+            if(i==1)
+            {
+                contenidoHTML+=`<td>
+                    <button class="btnnumero1" id="mas" value="+">+</button>
+                </td>` 
+            }
+            if(i==2)
+            {
+                contenidoHTML+=`<td>
+                    <button class="btnnumero1" id="menos" value="-">-</button>
+                </td>` 
+            }
+            if(i==3)
+            {
+                contenidoHTML+=`<td>
+                    <button class="btnnumero1" id="por" value="*">*</button>
+                </td>` 
+            }
+
         }
+        contenidoHTML+=`<tr>
+        <td>
+            <button class="btnnumero1" id="punto" value=".">.</button>
+        </td>
+        <td>
+            <button class="btnnumero1" id="cero" value="0">0</button>
+        </td>
+        <td>
+            <button class="btnnumero1" id="igual">=</button>
+        </td>
+        <td>
+            <button class="btnnumero1" id="div" value="/">/</button>
+        </td>
+        </tr>
+        <tr>
+        <td colspan="5">
+            <button id="borrar">BORRAR</button>
+        </td>
+        </tr>`
+
+        
         contenidoHTML+="</table>"
-      
-        contenedor.innerHTML=contenidoHTML
+        
+        
+
+        divcontenedor.innerHTML=contenidoHTML
+        
+
+        document.querySelectorAll(".btnnumero").forEach(x => { 
+
+            x.addEventListener('click',function(){
+                divresultado.innerHTML=divresultado.innerHTML + x.value
+            })
+
+         })
+
+         punto.addEventListener("click",function(){
+
+            divresultado.innerHTML=divresultado.innerHTML + punto.value
+         })
+         cero.addEventListener("click",function(){
+
+            divresultado.innerHTML=divresultado.innerHTML + cero.value
+         })
+         mas.addEventListener("click",function(){
+
+            divresultado.innerHTML=divresultado.innerHTML + mas.value
+         })
+         por.addEventListener("click",function(){
+
+            divresultado.innerHTML=divresultado.innerHTML + por.value
+         })
+         menos.addEventListener("click",function(){
+
+            divresultado.innerHTML=divresultado.innerHTML + menos.value
+         })
+         div.addEventListener("click",function(){
+
+            divresultado.innerHTML=divresultado.innerHTML + div.value
+         })
+         igual.addEventListener("click",function(){
+
+            divresultado.innerHTML=eval(divresultado.innerHTML)
+         })
+         borrar.addEventListener("click",function(){
+            let nuevo=""
+            divresultado.innerHTML=nuevo
+         })
+
+         
+    })
 
 
 })
