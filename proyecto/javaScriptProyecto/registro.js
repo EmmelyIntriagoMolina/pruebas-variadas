26bd887fde4f5c5bb5fb53db5875127045e206d6
Emmely Intriago <gabyintriago07@gmail.com>
2020-07-06 11:19:46 -0500

    contenido proyecto

--- proyecto/javaScriptProyecto/registro.js
+++ proyecto/javaScriptProyecto/registro.js
@@ -1,19 +1,37 @@
 window.addEventListener('load', function(){
 
-    function Login()
-    { 
-        var done=0; 
-        var usuario=document.login.usuario.value; 
-        var password=document.login.password.value; 
-        if (usuario=="USUARIO1" && password=="CONTRASEÑA1") { 
-        window.location="TU_PAGINA_WEB.HTML"; 
-        } 
-        if (usuario=="USUARIO2" && password=="CONTRASEÑA2") { 
-        window.location="TU_PAGINA_WEB.HTML"; 
-        } 
-        if (usuario=="" && password=="") { 
-        window.location="errorpopup.html"; 
-        } 
-    } 
+    
+    txtcedula.addEventListener('blur', function()
+    {
+        mensajeCedula.innerHTML=""
+        if(txtcedula.value.length!=10)
+        {
+            mensajeCedula.innerHTML+="El número de carcteres debe ser de 10 <br>"
+        }
+        if (!validarCedula(txtcedula.value))
+        {
+            mensajeCedula.innerHTML+="No cumple los criterios de validacion del Registro Civil <br>"
+        }
+    
+    })
 })
 
+    
+
+
+function validarCedula(parametro)
+{   
+    if(parametro.length!=10) return false;
+    let semaforo=false;
+    calculo=0;
+    parametro.substr(0,9).split('').forEach(element => { //dividir los numeros, ponerlos como un arreglo y recorrerlos
+        let numeroActual =semaforo ? parseInt(element): parseInt(element)*2;
+        calculo+=numeroActual>9 ? numeroActual-9: numeroActual;
+        semaforo=!semaforo;
+    })
+
+    while(calculo>0) calculo-=10;
+    return parseInt(parametro.substr(9)) + calculo==0;
+
+     
+}
